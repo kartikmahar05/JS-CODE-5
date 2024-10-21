f91c435cc3213ce21bc69780d8d862ed79cff052
Kartik <kartikmahar5@gmail.com>
2024-10-21 09:13:00 +0530

    basic js

---
+++ class.js
@@ -0,0 +1,23 @@
+
+/*console.log("Sum of a and b is",a+b)
+console.log("Multiplication of a and b is",a*b)
+console.log("Division of a and b is",a/b)
+console.log("Remainder of a and b is",a%b)*/
+
+                    //relational operator
+                    let a=51;
+                    let b=20;
+                    let c="51"
+                    console.log(a>b)
+                    console.log(b>a)
+                    console.log(a==b)
+                    console.log(a!=b)
+                    console.log(a===c)
+                                    //Assignment operator
+                    a+=4
+                    console.log(a)
+                    a%=6
+                    console.log(a)
+                    a++
+                    --a
+                    console.log(a)
