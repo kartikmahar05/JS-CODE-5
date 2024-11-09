ae77e1f3ae1c18a926a882ef6ce4ef9fe74094f7
KISHOR  SINGH <133872880+kartikmahar05@users.noreply.github.com>
2024-11-09 21:12:53 +0530

    Update class.js

--- class.js
+++ class.js
@@ -5,9 +5,9 @@ console.log("Division of a and b is",a/b)
 console.log("Remainder of a and b is",a%b)*/
 
                     //relational operator
-                    let a=51;
-                    let b=30;
-                    let c="41"
+                    let a=61;
+                    let b=40;
+                    let c="51"
                     console.log(a>b)
                     console.log(b>a)
                     console.log(a==b)
